836c7c3edff1ccd9cf3a6f7d7e4c7acfad94ad72
jicsoninfo <jicsoninfo@gmail.com>
2023-05-23 17:58:22 +0530

    singly linked list

--- resources/views/dsa/queue/js/queuejs.blade.php
+++ resources/views/dsa/queue/js/queuejs.blade.php
@@ -88,9 +88,44 @@ class Queuell{
     constructor(){
         this.first = null
         this.last = null
-        this.size = 0
+        this.size = 0;
+    }
+
+    enqueue(val){
+        var newNode = new Nodell(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        }else{
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue(){
+        if(!this.first) return null
+        var temp = this.first
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next
+        this.size--
+        return temp.value;
     }
 }
+
+const quickQueue = new Queuell;
+quickQueue.enqueue("50");
+quickQueue.enqueue("53");
+quickQueue.enqueue("57");
+quickQueue.enqueue("59");
+quickQueue.enqueue("62");
+
+
+quickQueue.dequeue();
+quickQueue.dequeue(); 
+console.log(quickQueue.first);
 
 
 
