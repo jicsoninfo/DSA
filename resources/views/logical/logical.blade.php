72bb622e7c2a407b7d67836552b87f4e380816a6
jicsoninfo <jicsoninfo@gmail.com>
2023-05-30 15:52:47 +0530

    Added in logical selection sort

--- resources/views/logical/logical.blade.php
+++ resources/views/logical/logical.blade.php
@@ -39,6 +39,37 @@ function reversDigits($num){
     return $rev_num;
 }
 
+
+echo "<br>==================================</br>";
+echo "<br>below program sorting without function selection sort</br>";
+$arr_selection = [65, 76, 5, 12, 22, 11, 10];
+$total_len = count($arr_selection);
+//$sorted_arr = [];
+
+
+    function selection_sort($arr_selection, $len){
+        for($i=0;$i<$len;$i++){
+            $low=$i;
+            for($j=$i+1;$j<$len;$j++){
+                if($arr_selection[$j]<$arr_selection[$low]){
+                    $low = $j;
+                }
+                if($arr_selection[$i]>$arr_selection[$low]){
+                    $temp = $arr_selection[$i];
+                    $arr_selection[$i] = $arr_selection[$low];
+                    //array_push($sorted_arr, $arr_selection[$low]);
+                    $arr_selection[$low] = $temp;
+                    //array_push($sorted_arr, $temp);
+                }
+            }
+        }
+        return $arr_selection;
+    }
+$after_sort = selection_sort($arr_selection, $total_len);
+echo "<pre>";
+print_r($after_sort);
+
+echo "<br>==================================</br>";
 ?>
 
 <script>
