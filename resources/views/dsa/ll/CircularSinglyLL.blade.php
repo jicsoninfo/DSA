65a7a60296a7d4722e9757ee000ba2e43bf269e0
jicsoninfo <jicsoninfo@gmail.com>
2023-06-20 18:59:51 +0530

    circular ll traverse

--- resources/views/dsa/ll/CircularSinglyLL.blade.php
+++ resources/views/dsa/ll/CircularSinglyLL.blade.php
@@ -12,6 +12,26 @@ class csll{
     {
         $this->head = null;
     }
+
+    //display the content of the list
+    public function PrintList() {
+        $temp = new CSNode();
+        $temp = $this->head;
+        if($temp != null) {
+        echo "The list contains: <br />";
+        while(true) {
+            //echo $temp->data." ";
+            echo $temp->data."<br />";
+            $temp = $temp->next;
+            if($temp == $this->head)
+            break;        
+        }
+        echo "\n";
+        } else {
+        echo "The list is empty.\n";
+        }
+    }
+
 }
 
 $mycsll = new csll();
@@ -19,16 +39,23 @@ $mycsll = new csll();
 $csll0 = new CSNode();
 
 $csll0->data = 10;
-$csll0->next = null;
+//$mycsll->head = $csll0;
+$csll0->next = $mycsll->head;
 $mycsll->head = $csll0;
+//$mycsll->head = $csll0;
 
 $csll1 = new CSNode();
 $csll1->data = 14;
-$csll1->next = null;
+$csll1->next = $mycsll->head;
 $csll0->next = $csll1;
 
+$csll2 = new CSNode();
+$csll2->data = 18;
+$csll2->next = $mycsll->head;
+$csll1->next = $csll2;
 
-echo "<pre>";
-print_r($mycsll);
+$mycsll->PrintList();
+// echo "<pre>";
+// print_r($mycsll);
 
 ?>
