daf2f681fd6872f7cc890142b6d72283cf991f40
jicsoninfo <jicsoninfo@gmail.com>
2023-06-23 18:43:13 +0530

    circular singly linked list added

--- resources/views/dsa/ll/CircularDoublyLL.blade.php
+++ resources/views/dsa/ll/CircularDoublyLL.blade.php
@@ -1,4 +1,36 @@
 <?php
-echo "Circular Doubly Singly LL";
+echo "Circular Doubly LL";
 
+class CDLLNode{
+    public $data;
+    public $next;
+    public $prev;
+}
+
+class cdll{
+    public $head;
+    public function __construct()
+    {
+        $this->head = null;
+    }
+}
+
+$cdll = new cdll();
+
+$cdll01 = new CDLLNode();
+$cdll01->data = 13;
+//$cdll01->next = null;
+//$cdll01->prev = null;
+$cdll->head = $cdll01;
+$cdll01->next = $cdll->head;
+$cdll01->prev = $cdll->head;
+
+$cdll02 = new CDLLNode();
+$cdll02->data = 16;
+$cdll02->prev = $cdll01;
+$cdll01->next = $cdll02;
+$cdll02->prev = $cdll->head;
+
+echo "<pre>";
+print_r($cdll);
 ?>
