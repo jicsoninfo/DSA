15e5e89f031036976af163334df5fa05a1367585
jicsoninfo <jicsoninfo@gmail.com>
2023-07-27 15:50:25 +0530

    added method PrintList, push_front, push_back and push_at added in DLL

--- resources/views/dsa/ll/DoublyLL.blade.php
+++ resources/views/dsa/ll/DoublyLL.blade.php
@@ -1,5 +1,5 @@
 <?php
-echo "Doubly Singly LL";
+echo "Doubly LL";
 
     class DNode{
         public $data;
@@ -13,31 +13,160 @@ echo "Doubly Singly LL";
         {
          $this->head=null;   
         }
+
+
+        //Display the content of the list
+        public function PrintList(){
+          $temp = new DNode();
+          $temp = $this->head;
+          if($temp != null){
+            echo "The list contains: <br>";
+            while($temp != null){
+                echo $temp->data. " <br>";
+                $temp = $temp->next;
+            }
+            echo "<br>";
+          }else{
+            echo "The list is empty.\n";
+          }  
+        }
+
+        //Add new element at the start of the list
+        public function push_front($newElement){
+            $newNode = new DNode();
+            $newNode->data = $newElement;
+            $newNode->next = null;
+            $newNode->prev = null;
+            if($this->head == null){
+                $this->head = $newNode;
+            }else{
+                //$newNode->next = $this->head->next;
+                $this->head->prev = $newNode;
+                $newNode->next = $this->head;
+                $this->head = $newNode;
+            }
+        }
+
+        //Add new element at the end of the list DLL
+        public function push_back($newElement){
+            $newNode = new DNode();
+            $newNode->data = $newElement;
+            $newNode->next = null;
+            $newNode->prev = null;
+            if($this->head == null){
+                $this->head = $newNode;
+            }else{
+                $temp = new DNode();
+                $temp = $this->head;
+                while($temp->next != null){
+                    $temp = $temp->next;
+                }
+                $temp->next = $newNode;
+                $newNode->prev = $temp;
+            }
+        }
+
+        //Insert a new element at the given position
+        public function pust_at($newElement, $position){
+            $newNode = new DNode();
+            $newNode->data = $newElement;
+            $newNode->next = null;
+            $newNode->prev = null;
+
+            if($position < 1){
+                echo "\nposition should be >= 1.";
+            }else if($position == 1){
+                    $this->head->prev = $newNode;
+                    $newNode->next = $this->head;
+                    $this->head = $newNode;
+                }
+            else{
+                $temp = new DNode();
+                $temp = $this->head;
+                for($i=1; $i < $position-1; $i++){
+                    if($temp != null){
+                        $temp = $temp->next;
+                    }
+                }
+                if($temp != null){
+                    $newNode->next = $temp->next;
+                    $newNode->prev = $temp;
+                    $temp->next = $newNode;
+                    if($newNode->next != null){
+                        $newNode->next->prev = $newNode;
+                    }else{
+                        echo "\n The previious node is null";
+                    }
+                }else{
+                    echo "\n The position is greater then total node is null";
+                }
+            }
+            
+        }
+
+
+
+
+
     }
+
+    
 
 
     $dll = new dll();
 
-    $dn01 = new DNode();
-    $dn01->data = 9;
-    $dn01->next = null;
-    $dn01->prev =  null;
-    $dll->head = $dn01;
+    // $dn01 = new DNode();
+    // $dn01->data = 9;
+    // $dn01->next = null;
+    // $dn01->prev =  null;
+    // $dll->head = $dn01;
 
-    $dn02 = new DNode();
-    $dn02->data = 13;
-    $dn02->next = null;
-    $dn02->prev = $dn01;
-    $dn01->next = $dn02;
-    //$dll->head = $dn01;
+    // $dn02 = new DNode();
+    // $dn02->data = 13;
+    // $dn02->next = null;
+    // $dn02->prev = $dn01;
+    // $dn01->next = $dn02;
+    // //$dll->head = $dn01;
 
-    $dn03 = new DNode();
-    $dn03->data = 17;
-    $dn03->next = null;
-    $dn03->prev =  $dn02;
-    $dn02->next = $dn03;
-    //$dll->head = $dn01;
+    // $dn03 = new DNode();
+    // $dn03->data = 17;
+    // $dn03->next = null;
+    // $dn03->prev =  $dn02;
+    // $dn02->next = $dn03;
+    // //$dll->head = $dn01;
 
-    echo "<pre>";
-    print_r($dll);
+
+
+$data = ['91', '81', '72', '63', '54', '45', '36', '63'];
+$ll1 = "";
+$ll2 = "";
+foreach($data as $key=>$value){
+    $ll1 = 'll'.$key;
+    if($key == 0){
+    $ll1 = new DNode();
+    $ll1->data = $value;
+    $dll->head = $ll1;
+    $ll1->next =  null;
+    $ll1->prev = null;
+    $ll2 = $ll1;
+    }
+    else{
+        $ll1 = new DNode();
+        $ll1->data = $value;
+        $ll1->next = null;
+        $ll1->prev = $ll2;
+        $ll2->next = $ll1;
+        $ll2= $ll1;
+    }
+    //print_r($ll2->next);
+}
+    // echo "<pre>";
+    // print_r($dll);
+
+    // $dll->push_front(101);
+    // $dll->push_back(103);
+    $dll->pust_at(105,20);
+    $dll->PrintList();
+    
+    
 ?>
--- resources/views/dsa/test.blade.php
+++ resources/views/dsa/test.blade.php
@@ -1927,4 +1927,196 @@ public function push_at($newElement, $position) {
   }   
 
 
+  //create an empty LinkedList
+//$MyList = new LinkedList();
+
+//Add first node.
+$first = new Node();
+$first->data = 10;
+$first->next = null;
+$first->prev = null;
+//linking with head node
+$MyList->head = $first;
+
+//Add second node.
+$second = new Node();
+$second->data = 20;
+$second->next = null;
+//linking with first node
+$second->prev = $first;
+$first->next = $second;
+
+//Add third node.
+$third = new Node();
+$third->data = 30;
+$third->next = null;
+//linking with second node
+$third->prev = $second;
+$second->next = $third;
+
+
+ //display the content of the list
+  public function PrintList() {
+    $temp = new Node();
+    $temp = $this->head;
+    if($temp != null) {
+      echo "The list contains: ";
+      while($temp != null) {
+        echo $temp->data." ";
+        $temp = $temp->next;
+      }
+      echo "\n";
+    } else {
+      echo "The list is empty.\n";
+    }
+  }   
+
+  //Add new element at the start of the list
+  public function push_front($newElement) {
+    $newNode = new Node();
+    $newNode->data = $newElement;
+    $newNode->next = null;
+    $newNode->prev = null; 
+    if($this->head == null) {
+      $this->head = $newNode;
+    } else {
+      $this->head->prev = $newNode;
+      $newNode->next = $this->head;
+      $this->head = $newNode;
+    }    
+  }
+
+  //Add new element at the end of the list
+  public function push_back($newElement) {
+    $newNode = new Node();
+    $newNode->data = $newElement;
+    $newNode->next = null;
+    $newNode->prev = null; 
+    if($this->head == null) {
+      $this->head = $newNode;
+    } else {
+      $temp = new Node();
+      $temp = $this->head;
+      while($temp->next != null) {
+        $temp = $temp->next;
+      }
+      $temp->next = $newNode;
+      $newNode->prev = $temp;
+    }    
+  }
+
+  //Inserts a new element at the given position
+  public function push_at($newElement, $position) {     
+    $newNode = new Node(); 
+    $newNode->data = $newElement;
+    $newNode->next = null;
+    $newNode->prev = null;
+    if($position < 1) {
+      echo "\nposition should be >= 1.";
+    } else if ($position == 1) {
+      $newNode->next = $this->head;
+      $this->head->prev = $newNode;
+      $this->head = $newNode;
+    } else {
+      $temp = new Node();
+      $temp = $this->head;
+      for($i = 1; $i < $position-1; $i++) {
+        if($temp != null) {
+          $temp = $temp->next;
+        }
+      }
+      if($temp != null) {
+        $newNode->next = $temp->next;
+        $newNode->prev = $temp;
+        $temp->next = $newNode; 
+        if($newNode->next != null)
+          $newNode->next->prev = $newNode; 
+      } else {
+        echo "\nThe previous node is null.";
+      }       
+    }
+  }  
+
+
+  //Delete first node of the list
+  public function pop_front() {
+    if($this->head != null) {
+      $temp = $this->head;
+      $this->head = $this->head->next;
+      $temp = null;
+      if($this->head != null) 
+        $this->head->prev = null;        
+    }
+  }
+
+
+  //Delete last node of the list
+  public function pop_back() {
+    if($this->head != null) {
+      if($this->head->next == null) {
+        $this->head = null;
+      } else {
+        $temp = new Node();
+        $temp = $this->head;
+        while($temp->next->next != null)
+          $temp = $temp->next;
+        $lastNode = $temp->next;
+        $temp->next = null; 
+        $lastNode = null;
+      }
+    }
+  }
+
+  //Delete an element at the given position
+  public function pop_at($position) {     
+    if($position < 1) {
+      echo "\nposition should be >= 1.";
+    } else if ($position == 1 && $this->head != null) {
+      $nodeToDelete = $this->head;
+      $this->head = $this->head->next;
+      $nodeToDelete = null;
+      if ($this->head != null)
+        $this->head->prev = null;
+    } else {
+      $temp = new Node();
+      $temp = $this->head;
+      for($i = 1; $i < $position-1; $i++) {
+        if($temp != null) {
+          $temp = $temp->next;
+        }
+      }
+      if($temp != null && $temp->next != null) {
+        $nodeToDelete = $temp->next;
+        $temp->next = $temp->next->next;
+        if($temp->next->next != null)
+          $temp->next->next->prev = $temp->next;   
+        $nodeToDelete = null; 
+      } else {
+        echo "\nThe node is already null.";
+      }       
+    }
+  } 
+
+  //delete all nodes of the list
+  public function deleteAllNodes() {
+    $temp = new Node();
+    while($this->head != null) {
+      $temp = $this->head;
+      $this->head = $this->head->next;
+      $temp = null;
+    }
+    echo "All nodes are deleted successfully.\n";
+  }   
+
+  //count nodes in the list
+  public function countNodes() {
+    $temp = new Node();
+    $temp = $this->head;
+    $i = 0;
+    while($temp != null) {
+      $i++;
+      $temp = $temp->next;
+    }
+    return $i;  
+  }  
 ?>
