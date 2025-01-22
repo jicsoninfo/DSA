2a3dceaeb0a6319b2bc26a9b2c9d1915dc24b73f
Inforavibhushan <ravi.bhushan@infoicon.in>
2025-01-22 17:42:27 +0530

    added double linked list with tail example

--- resources/views/dsa/ll/dll/01Doublyll.blade.php
+++ resources/views/dsa/ll/dll/01Doublyll.blade.php
@@ -158,7 +158,7 @@ echo "double linked list" ."<br>";
   //=======================================================================================
   echo "=====================================================" ."<br>";
   echo "Double linked list with tail variable" ."<br>";
-/*
+
     class DllWithTail{
         public $head;
         public $tail;
@@ -169,11 +169,11 @@ echo "double linked list" ."<br>";
 
         //Insert a node at the end (push back)
         public function dll_push_back($data){
-            $newNode = new nodedll();
+            $newNode = new nodedll($data);
             //if the list is empty
             if($this->head == null){
                 $this->head = $newNode;
-                $this->tail = $nreNode; //Tail also point to the new node
+                $this->tail = $newNode; //Tail also point to the new node
             }else{
                 //Traverse to the end of the list
                 $temp = $this->tail;
@@ -185,121 +185,73 @@ echo "double linked list" ."<br>";
         
         //Insert a node at the front (pust_front)
         public function dll_push_front($data){
-            
+            $newNode = new nodedll($data);
+
+            //If the list is empty
+            if($this->head == null){
+                $this->head = $newNode;
+                $this->tail = $newNode; //Tail points to the node as well
+            }else{
+                $newNode->next = $this->head; // link the new node to the current head
+                $this->head->prev = $newNode; //Set teh lod head's prev to the new node
+                $this->head = $newNode; //Update the head to the new node
+            }
+        }
+
+        //Print the list from the head
+        public function dll_print(){
+            if($this->head !== null){
+                $temp = $this->head;
+                while($temp !== null){
+                    echo $temp->data . "<br>";
+                    $temp = $temp->next;
+                }
+            }else{
+                echo "List is empty";
+            }
+        }
+
+        //Print the list from the tail (optional)
+        public function dll_print_reverse(){
+            if($this->tail !== null){
+                $temp = $this->tail;
+                while ($temp !== null){
+                    echo $temp->data . "<br>";
+                    $temp = $temp->prev;
+                }
+            }else{
+                echo "List is empty";
+            }
         }
 
 
 
     }
 
-    
+    //Example Usage
+    $newdlltail = new DllWithTail();
 
+    // Insert nodes at the end
+    $newdlltail->dll_push_back(20);
+    $newdlltail->dll_push_back(30);
+    $newdlltail->dll_push_back(15);
+    $newdlltail->dll_push_back(35);
+    $newdlltail->dll_push_back(39);
 
-*/
-// class Node {
-//     public $data;
-//     public $next;
-//     public $prev;
+    // Insert nodes at the front
+    $newdlltail->dll_push_front(40);
+    $newdlltail->dll_push_front(45);
+    $newdlltail->dll_push_front(50);
 
-//     public function __construct($data) {
-//         $this->data = $data;
-//         $this->next = null;
-//         $this->prev = null;
-//     }
-// }
+    // Print the list from head to tail
+    echo "List from head to tail:<br>";
+    $newdlltail->dll_print();
 
-// class DLL {
-//     public $head;
-//     public $tail;
+    echo "<br>";
 
-//     public function __construct() {
-//         $this->head = null;
-//         $this->tail = null;
-//     }
-
-//     // Insert a node at the end (push_back)
-//     public function dll_push_back($data) {
-//         $newNode = new Node($data);
-        
-//         // If the list is empty
-//         if ($this->head == null) {
-//             $this->head = $newNode;
-//             $this->tail = $newNode; // Tail also points to the new node
-//         } else {
-//             // Traverse to the end of the list
-//             $temp = $this->tail;
-//             $temp->next = $newNode; // Link the last node to the new node
-//             $newNode->prev = $temp; // Set the new node's prev to the last node
-//             $this->tail = $newNode; // Update the tail to the new node
-//         }
-//     }
-
-//     // Insert a node at the front (push_front)
-//     public function dll_push_front($data) {
-//         $newNode = new Node($data);
-        
-//         // If the list is empty
-//         if ($this->head == null) {
-//             $this->head = $newNode;
-//             $this->tail = $newNode; // Tail points to the new node as well
-//         } else {
-//             $newNode->next = $this->head; // Link the new node to the current head
-//             $this->head->prev = $newNode; // Set the old head's prev to the new node
-//             $this->head = $newNode; // Update the head to the new node
-//         }
-//     }
-
-//     // Print the list from the head
-//     public function dll_print() {
-//         if ($this->head !== null) {
-//             $temp = $this->head;
-//             while ($temp !== null) {
-//                 echo $temp->data . "<br>";
-//                 $temp = $temp->next;
-//             }
-//         } else {
-//             echo "List is empty";
-//         }
-//     }
-
-//     // Print the list from the tail (optional)
-//     public function dll_print_reverse() {
-//         if ($this->tail !== null) {
-//             $temp = $this->tail;
-//             while ($temp !== null) {
-//                 echo $temp->data . "<br>";
-//                 $temp = $temp->prev;
-//             }
-//         } else {
-//             echo "List is empty";
-//         }
-//     }
-// }
-
-// // Example Usage
-// $newsdll = new DLL();
-
-// // Insert nodes at the end
-// $newsdll->dll_push_back(20);
-// $newsdll->dll_push_back(30);
-// $newsdll->dll_push_back(15);
-// $newsdll->dll_push_back(35);
-// $newsdll->dll_push_back(39);
-
-// // Insert nodes at the front
-// $newsdll->dll_push_front(40);
-// $newsdll->dll_push_front(45);
-// $newsdll->dll_push_front(50);
-
-// // Print the list from head to tail
-// echo "List from head to tail:<br>";
-// $newsdll->dll_print();
-
-// echo "<br>";
-
-// // Print the list from tail to head (optional)
-// echo "List from tail to head:<br>";
-// $newsdll->dll_print_reverse();
+    // Print the list from tail to head (optional)
+    echo "List from tail to head:<br>";
+    $newdlltail->dll_print_reverse();
 
 
 
